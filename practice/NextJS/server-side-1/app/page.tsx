f85f5d24302dcb38bdf691fa9e476f515125c33e
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-07-01 23:49:40 +0530

    Understanding backend routing

---
+++ practice/NextJS/server-side-1/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import axios from "axios";
+import { useEffect } from "react";
+
+
+
+export default async function Home() {
+  
+  return (
+    <div>
+      hi there
+    </div>
+  );
+}
