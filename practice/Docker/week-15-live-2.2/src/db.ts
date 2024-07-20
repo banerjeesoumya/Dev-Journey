db0e8c0499b380ef09b28c6e0fa219cdc5062048
Turbobot <turbobot@vercel.com>
2024-07-20 22:10:20 +0530

    feat(create-turbo): apply official-starter transform

--- practice/Docker/week-15-live-2.2/src/db.ts
+++ practice/Docker/week-15-live-2.2/src/db.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema, model } from 'mongoose';
 
-const mongoUrl: string = 'mongodb://my_mongo:27017/myDatabase';
+const mongoUrl: string = process.env.MONGO_URL || 'mongodb://localhost:27017/myDatabase';
 
 // Connect to MongoDB
 mongoose.connect(mongoUrl)
