05f35859b3de8f9217add4ae994ab44c003b5d6b
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-12-23 21:53:39 +0530

    Updated server code

--- practice/Redis/leetcode-clone/express-server/src/index.ts
+++ practice/Redis/leetcode-clone/express-server/src/index.ts
@@ -1,24 +1,48 @@
-import express from "express"
-import { createClient } from "redis"
+import express from "express";
+import { createClient } from "redis";
 
 const app = express();
 app.use(express.json());
 
 const client = createClient();
-client.connect();
+client.on("error", (err) => {
+    console.log("Redis Client error", err)
+})
+
+async function Server() {
+    try {
+        await client.connect();
+        console.log("Connected to Redis")
+        
+        app.listen(3000, () => {
+            console.log("Server is running on port 3000")
+        });
+    } catch (err) {
+        console.log("Failed to connect to Redis", err)
+    }
+}
 
 app.post("/submit", async (req, res) => {
-    const {problemId, userId, code, language} = req.body;
-
-    await client.lPush("submissions", JSON.stringify({
+    const {
         problemId,
         userId,
         code,
         language
-    }));
-    res.json({
-        message: "Submission received"
-    })
+    } = req.body;
+
+    try {
+        await client.lPush("submissions", JSON.stringify({
+            problemId, userId, code, language
+        }));
+        res.status(200).send({
+            message: "Submission received succesfully"
+        })
+    } catch (err) {
+        console.log("Error", err);
+        res.status(500).send({
+            message: "An error occured"
+        })
+    }
 })
 
-app.listen(3000)
+Server()
