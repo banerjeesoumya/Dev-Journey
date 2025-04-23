623ceb3cf43f670b9c4cd8345124368a01fca5d1
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2025-04-23 12:42:32 +0530

    Started with the basics

---
+++ learning_golang/main.go
@@ -0,0 +1,11 @@
+package main 
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("Random number:", rand.Intn(100))
+}
---
+++ learning_golang/random/rand.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main () {
+	fmt.Println("Program to find a random number")
+	fmt.Println("Random Number:", rand.Intn(100))
+}
