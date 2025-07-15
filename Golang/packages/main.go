9d69d8334349e9114a8efa8ea06ac00d1874ff9f
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2025-07-15 15:05:43 +0530

    Learning basics

---
+++ Golang/packages/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"packages/math"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("This is a simple Go program.")
+	fmt.Printf("Addition using the math package: %v\n", math.Add(5, 3))
+}
